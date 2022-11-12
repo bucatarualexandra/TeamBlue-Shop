d1a83795e1d6bbcc108dddfd59a7ddef06fdc882
Sorin Costiuc <110764597+SorinCostiuc@users.noreply.github.com>
2022-11-12 12:20:40 +0200

    2. Fixing cfg + run main and create tables

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -3,10 +3,14 @@ import org.hibernate.cfg.Configuration;
 
 public class MainPage {
     public static void main(String[] args) {
+        System.out.println("Hello");
 
     }
 
     static SessionFactory sf = new Configuration()
             .configure("hibernate.cfg.xml")
             .buildSessionFactory();
+
+
 }
+
