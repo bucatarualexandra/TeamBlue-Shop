cd05f2457934011c0168997ddac45bab2f636127
Sorin Costiuc <110764597+SorinCostiuc@users.noreply.github.com>
2022-11-13 14:50:46 +0200

    9. Creating menu

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -8,42 +8,71 @@ import user.User;
 import user.UsersManagement;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainPage {
+    public static void showMenu() {
+        System.out.println("0. Select user");
+        System.out.println("1. Add item");
+        System.out.println("2. Remove item");
+        System.out.println("3. Buy");
+        Scanner scanner = new Scanner(System.in);
+        String result = scanner.nextLine();
+
+        switch (result) {
+            case "0":
+                UsersManagement usersManagement = new UsersManagement();
+                usersManagement.getItems().forEach((user) -> System.out.println(user.getUserName()));
+                scanner.next();
+            case "1":
+
+            case "2":
+            case "3":
+            default:
+                System.out.println("Invalid option");
+        }
+
+    }
+
+    public static void add() {
+
+    }
+
+
     public static void main(String[] args) {
-        Storage storageTv = new Storage();
-        storageTv.setStock(8);
-
-        Storage storagePhone = new Storage();
-        storagePhone.setStock(2);
-
-        Storage storageFridge = new Storage();
-        storageFridge.setStock(3);
+//        Storage storageTv = new Storage();
+//        storageTv.setStock(8);
 //
-        Item tv = new Item();
-        tv.setProductName("TV");
-        tv.setDescription("8k");
-        tv.setPrice(2500.0);
-        tv.setOrderItem(new ArrayList<>());
-        tv.setStorage(storageTv);
-
-        storageTv.setItem(tv);
+//        Storage storagePhone = new Storage();
+//        storagePhone.setStock(2);
 //
-        Item phone = new Item();
-        phone.setProductName("Phone");
-        phone.setDescription("Smartphone");
-        phone.setPrice(1500.0);
-        phone.setStorage(storagePhone);
-
-        storagePhone.setItem(phone);
-
-        Item fridge = new Item();
-        fridge.setProductName("Fridge");
-        fridge.setDescription("Cooler A++");
-        fridge.setPrice(3500.0);
-        fridge.setStorage(storageFridge);
-
-        storageFridge.setItem(fridge);
+//        Storage storageFridge = new Storage();
+//        storageFridge.setStock(3);
+////
+//        Item tv = new Item();
+//        tv.setProductName("TV");
+//        tv.setDescription("8k");
+//        tv.setPrice(2500.0);
+//        tv.setOrderItem(new ArrayList<>());
+//        tv.setStorage(storageTv);
+//
+//        storageTv.setItem(tv);
+////
+//        Item phone = new Item();
+//        phone.setProductName("Phone");
+//        phone.setDescription("Smartphone");
+//        phone.setPrice(1500.0);
+//        phone.setStorage(storagePhone);
+//
+//        storagePhone.setItem(phone);
+//
+//        Item fridge = new Item();
+//        fridge.setProductName("Fridge");
+//        fridge.setDescription("Cooler A++");
+//        fridge.setPrice(3500.0);
+//        fridge.setStorage(storageFridge);
+//
+//        storageFridge.setItem(fridge);
 
 
 //        Product tv = new Product();
@@ -74,17 +103,18 @@ public class MainPage {
 //        UsersManagement usersManagement = new UsersManagement();
 //        usersManagement.insert(user2);
 //        usersManagement.getItems();
-        System.out.println("hello");
+//        System.out.println("hello");
+//
+////        OrderManagement orderManagement = new OrderManagement();
+////        orderManagement.getById(1);
+//        OrderItem orderItem = new OrderItem();
+//        if (storageTv.getStock() >= 1) {
+//            orderItem.addItemsToOrder(tv);
+//            storageTv.decrement();
+//        } else {
+//            System.out.println("Sorry, item not in store");
+//        }
 
-//        OrderManagement orderManagement = new OrderManagement();
-//        orderManagement.getById(1);
-        OrderItem orderItem = new OrderItem();
-        if (storageTv.getStock() >= 1) {
-            orderItem.addItemsToOrder(tv);
-            storageTv.decrement();
-        } else {
-            System.out.println("Sorry, item not in store");
-        }
 
     }
 }
